c9c8217212d616e6ff9f1edfaeef1ca260453dab
alejandro_vaz <alejandro.vaz.myt@gmail.com>
2026-01-02 11:48:20 +0100

    removed gpt

--- src/pages/Printables.tsx
+++ src/pages/Printables.tsx
@@ -19,7 +19,6 @@ export default function Printables(): React.ReactNode {
     return (
         <>
             <Section name="Cork board">
-                <Card type="pdf" to="/Printables/gpt.pdf">GPT conversations organization</Card>
                 <Card type="pdf" to="/Printables/habittracker.pdf">Habit tracker</Card>
                 <Card type="pdf" to="/Printables/s-i.pdf">Task management list</Card>
                 <Card type="pdf" to="/Printables/social.pdf">Network social interactions</Card>
