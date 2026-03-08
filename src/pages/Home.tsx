5a4d4e7abdb6887c7e42ab5478a94bb9cee9153e
alejandro-vaz <alejandro.vaz.myt@gmail.com>
2026-03-08 10:59:09 +0100

    details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,8 +22,8 @@ function Header(): React.ReactNode {
             <img src="/Home/profile.jpg" className="w-44 h-44 ml-auto mr-auto rounded-full mb-2"/>
             <h1 className="text-2xl font-bold text-slate-800 ml-auto mr-auto">Alejandro Vaz</h1>
             <p>Public email: <Link to="mailto:alejandro.vaz.myt@gmail.com" className="hover:underline text-slate-600">alejandro.vaz.myt@gmail.com</Link></p>
-            <p>Academic emails: <Link to="mailto:alejandro.vaz@educa.madrid.org" className="hover:underline text-slate-600">alejandro.vaz@educa.madrid.org</Link> <Link to="mailto:alv8@illinois.edu" className="hover:underline text-slate-600">alv8@illinois.edu</Link></p>
-            <p>Public number: <Link to="tel:+34679983643" className="hover:underline text-slate-600">+34 679 98 36 43</Link></p>
+            <p>Academic emails: <Link to="mailto:alejandro.vaz@educa.madrid.org" className="hover:underline text-slate-600">alejandro.vaz@educa.madrid.org</Link>, <Link to="mailto:alv8@illinois.edu" className="hover:underline text-slate-600">alv8@illinois.edu</Link></p>
+            <p>Public phone number: <Link to="tel:+34679983643" className="hover:underline text-slate-600">+34 679 98 36 43</Link></p>
         </>
     )
 }
