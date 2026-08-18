4d5300f28768225f3bb43e321497b3a115e204d5
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-08-18 12:54:01 +0200

    fix: reformated markdown

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -14,15 +14,13 @@ import Markdown from "react-markdown";
 //^
 
 //> RESEARCH -> FUNCTION
-export default function Research(): React.ReactNode {
-    return <Markdown>
-        ## Environment-Sensitive Lexicographic Disambiguation for Contextual Parsing
+export default function Research(): React.ReactNode {return <Markdown>
+## Environment-Sensitive Lexicographic Disambiguation for Contextual Parsing
 
-        Developed a novel contextual disambiguation algorithm for generalized parsers and
-        formalized it in a paper.
+Developed a novel contextual disambiguation algorithm for generalized parsers and
+formalized it in a paper.
 
-        - Pre-print available on arXiv and Zenodo.
-        - Submitted to the ACM Software Language Engineering 2026 conference.
-        - Implemented along the Mathsys Earley parser.
-    </Markdown>
-}
+- Pre-print available on arXiv and Zenodo.
+- Submitted to the ACM Software Language Engineering 2026 conference.
+- Implemented along the Mathsys Earley parser.
+</Markdown>}
